9f177a2f8d65a5cfede09ac6bfbfb6e87ef8192c
Ehtisham <syedehtishamshah001@gmail.com>
2024-08-10 02:58:14 +0500

    agent profile page

---
+++ agentprofile.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Meta Tags -->
+    <meta name="description" content="Real Estate">
+    <meta name="keywords" content="">
+    <meta name="author" content="Unicoder">
+    <link rel="shortcut icon" href="images/favicon.ico">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
+
+    <!-- Css Link -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-slider.css">
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+    <link rel="stylesheet" type="text/css" href="css/layerslider.css">
+    <link rel="stylesheet" type="text/css" href="css/color.css" id="color-change">
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!-- Title -->
+    <title>Real Estate</title>
+</head>
+<body>
+
+<!-- Page Wrapper -->
+<div id="page-wrapper">
+    <div class="row"> 
+
+        <?php include("include/header.php");?>
+        <!-- Header Start -->
+        <!-- Header End -->
+
+        <!-- Banner -->
+        <div class="banner-full-row page-banner" style="background-image:url('images/breadcromb.jpg');">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2 class="page-name float-left text-white text-uppercase mt-1 mb-0"><b>Agent Detail</b></h2>
+                    </div>
+                    <div class="col-md-6">
+                        <nav aria-label="breadcrumb" class="float-left float-md-right">
+                            <ol class="breadcrumb bg-transparent m-0 p-0">
+                                <li class="breadcrumb-item text-white"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">Agent Detail</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Banner End -->
+
+        <!-- Property Grid -->
+        <div class="full-row">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="featured-thumb hover-zoomer mb-4">
+                                    <div class="overlay-black overflow-hidden position-relative"> 
+                                        <img src="admin/property/zillhms1.jpg" alt="Property Image">
+                                        <div class="sale bg-success text-white">For Rent</div>
+                                        <div class="price text-primary text-capitalize">$500,000 <span class="text-white">120 Sqft</span></div>
+                                    </div>
+                                    <div class="featured-thumb-data shadow-one">
+                                        <div class="p-4">
+                                            <h5 class="text-secondary hover-text-success mb-2 text-capitalize"><a href="#">Zillyhomes</a></h5>
+                                            <span class="location text-capitalize"><i class="fas fa-map-marker-alt text-success"></i> Karachi</span> 
+                                        </div>
+                                        <div class="px-4 pb-4 d-inline-block w-100">
+                                            <div class="float-left text-capitalize"><i class="fas fa-user text-success mr-1"></i>By Ehtisham</div>
+                                            <div class="float-right"><i class="far fa-calendar-alt text-success mr-1"></i> 10-8-2024</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Additional Property Listings -->
+
+                            <div class="col-md-6">
+                                <div class="featured-thumb hover-zoomer mb-4">
+                                    <div class="overlay-black overflow-hidden position-relative"> 
+                                        <img src="admin/property/zillhms2.jpg" alt="Property Image">
+                                        <div class="sale bg-success text-white">For Rent</div>
+                                        <div class="price text-primary text-capitalize">PKR 20000 <span class="text-white">500 Sqft</span></div>
+                                    </div>
+                                    <div class="featured-thumb-data shadow-one">
+                                        <div class="p-4">
+                                            <h5 class="text-secondary hover-text-success mb-2 text-capitalize"><a href="#">Zillyhomes 2</a></h5>
+                                            <span class="location text-capitalize"><i class="fas fa-map-marker-alt text-success"></i> Karachi</span> 
+                                        </div>
+                                        <div class="px-4 pb-4 d-inline-block w-100">
+                                            <div class="float-left text-capitalize"><i class="fas fa-user text-success mr-1"></i>By Ehtisham</div>
+                                            <div class="float-right"><i class="far fa-calendar-alt text-success mr-1"></i> 1-8-2024</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="featured-thumb hover-zoomer mb-4">
+                                    <div class="overlay-black overflow-hidden position-relative"> 
+                                        <img src="admin/property/zillhms3.jpg" alt="Property Image">
+                                        <div class="sale bg-success text-white">For Rent</div>
+                                        <div class="price text-primary text-capitalize">PKR 20000 <span class="text-white">500 Sqft</span></div>
+                                    </div>
+                                    <div class="featured-thumb-data shadow-one">
+                                        <div class="p-4">
+                                            <h5 class="text-secondary hover-text-success mb-2 text-capitalize"><a href="#">Zillyhomes 3</a></h5>
+                                            <span class="location text-capitalize"><i class="fas fa-map-marker-alt text-success"></i> Karachi</span> 
+                                        </div>
+                                        <div class="px-4 pb-4 d-inline-block w-100">
+                                            <div class="float-left text-capitalize"><i class="fas fa-user text-success mr-1"></i>By Ehtisham</div>
+                                            <div class="float-right"><i class="far fa-calendar-alt text-success mr-1"></i> 1-8-2024</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+                    
+                    <div class="col-lg-4">
+                        <!-- Agent Details Section -->
+                        <div class="sidebar-widget">
+                            <h4 class="double-down-line-left text-secondary position-relative pb-4 my-4">Agent Details</h4>
+                            <div class="agent-info">
+                                <img src="images/agent.jpg" alt="Agent Image" class="img-fluid mb-3">
+                                <h5 class="text-secondary">John Doe</h5>
+                                <p><strong>Bio:</strong> John Doe is a licensed real estate agent with over 10 years of experience in helping clients find their dream homes. He specializes in residential properties and is known for his excellent customer service and negotiation skills.</p>
+                                <p><strong>Contact Information:</strong></p>
+                                <ul class="list-unstyled">
+                                    <li><i class="fas fa-envelope text-success mr-2"></i> john.doe@example.com</li>
+                                    <li><i class="fas fa-phone-alt text-success mr-2"></i> +123-456-7890</li>
+                                    <li><i class="fas fa-map-marker-alt text-success mr-2"></i> 123 Real Estate St, City, Country</li>
+                                </ul>
+                                <p><strong>License Number:</strong> XYZ-12345</p>
+                                
+                                <!-- Rating Section -->
+                                <p><strong>Rating:</strong></p>
+                                <div class="rating mb-3">
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star text-warning"></i>
+                                    <i class="fas fa-star-half-alt text-warning"></i>
+                                    <span class="ml-2">(4.5/5 based on 120 reviews)</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+        <!-- Footer Start -->
+        <?php include("include/footer.php");?>
+        <!-- Footer End -->
+        
+        <!-- Scroll to Top -->
+        <a href="#" class="bg-secondary text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a> 
+    </div>
+</div>
+<!-- Wrapper End --> 
+
+<!-- Js Link -->
+<script src="js/jquery.min.js"></script> 
+<script src="js/greensock.js"></script> 
+<script src="js/layerslider.transitions.js"></script> 
+<script src="js/layerslider.kreaturamedia.jquery.js"></script> 
+<script src="js/popper.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/owl.carousel.min.js"></script> 
+<script src="js/tmpl.js"></script> 
+<script src="js/jquery.dependClass-0.1.js"></script> 
+<script src="js/draggable-0.1.js"></script> 
+<script src="js/jquery.slider.js"></script> 
+<script src="js/wow.js"></script> 
+<script src="js/custom.js"></script>
+</body>
+</html>
